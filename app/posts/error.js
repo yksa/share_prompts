1b199ac1d6eecb9037a6e689005124c1b65e346b
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-06-25 21:32:30 +0630

    Routing & Special Next.js Files

---
+++ app/posts/error.js
@@ -0,0 +1,8 @@
+"use client"; // error component must be client side rendering
+import React from "react";
+
+const error = () => {
+  return <div>error</div>;
+};
+
+export default error;
---
+++ app/posts/layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const layout = ({ children }) => {
+  return (
+    <div>
+      <div>Navigate to Top</div>
+      {children}
+      testing
+    </div>
+  );
+};
+
+export default layout;
