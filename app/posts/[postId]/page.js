53725fc85cb2e5219d0c88e29b4abe0f121b4a4a
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-06-28 14:12:20 +0630

    SEO & Metadata

--- app/posts/[postId]/page.js
+++ app/posts/[postId]/page.js
@@ -1,5 +1,11 @@
 // http://localhost:3000/posts/postId
 
+// dynamic metadata
+export async function generateMetadata({ params, searchParams }) {
+  const postId = params.postId;
+  return { title: `Post ${postId}` };
+}
+
 export default function Page({ params }) {
   return <div>My Post: {params.postId}</div>;
 }
