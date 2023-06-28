dfde37cb682f8a5427f42255c9ad3607f21189c6
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-06-28 11:27:51 +0630

    Next.js Api End Points

---
+++ app/api/users/route.js
@@ -0,0 +1,8 @@
+export async function GET(request) {
+  const users = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+    { id: 3, name: "Bob" },
+  ];
+  return new Response(JSON.stringify(users));
+}
